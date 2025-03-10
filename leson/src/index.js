c41a9ec222009aed18496b17515637fa03b4221a
tgme1122 <git1122hub@gmail.com>
2025-03-10 19:51:14 +0500

    tayyor

--- leson/src/index.js
+++ leson/src/index.js
@@ -1,4 +1,9 @@
 import React from "react";
 import ReactDom from "react-dom/client"
+import App from "./App";
 
-ReactDom.createRoot
+ReactDom.createRoot(document.getElementById('root')).render(
+    <div>
+        <App/>
+    </div>
+)
